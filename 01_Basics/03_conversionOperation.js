34df1106fbb0f89cde7f9d793fea0219ceb23cb1
r@hit_53 <95581133+rohitmak@users.noreply.github.com>
2023-08-20 11:41:06 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "RR"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score) // score is converted into number from string.
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "RR"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "RR" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);

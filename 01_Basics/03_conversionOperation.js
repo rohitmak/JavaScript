bf68978a87585caece7349a7547cb433f6b9cf1c
r@hit_53 <95581133+rohitmak@users.noreply.github.com>
2023-10-17 04:52:19 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -8,13 +8,13 @@ let valueInNumber = Number(score) // score is converted into number from string.
 // console.log(valueInNumber);
 
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN, but the type of converted one will be number.
 // true => 1; false => 0
 
 let isLoggedIn = "RR"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,8 +23,47 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 let newString = stringNumber + 20
-console.log(newString)
+// console.log(newString)
+
+// ******************** Operations ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 ** 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " Rohit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 3 + "3");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); // gives error as it cannot be performed
+// console.log(+ "");
+
+let x = 3;
+const y = x++; // y: 3, because postfix operator first assign value and then increment/decrement it.
+
+console.log(`x: ${x}, y: ${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a: ${a}, b: ${b}`);

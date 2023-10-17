b1bbc004fe10d252f6343f29449f820e9ae8e307
r@hit_53 <95581133+rohitmak@users.noreply.github.com>
2023-10-17 07:36:15 +0000

    stack & heap memory

--- 01_Basics/datatype_summary.js
+++ 01_Basics/datatype_summary.js
@@ -42,4 +42,30 @@ const myFunction = function() {
 }
 // typeof all reference data types are objects, like for function it's function object.
 
-console.log(typeof heroes);
+// console.log(typeof heroes);
+
+// *******************************************************************************
+
+// Stack(Primitive) & Heap(Non-Primitive) Memory
+// In case of Stack Memory we get the copy of datatype whereas in Heap we get the reference to the same memory, so any changes made in primitive datatypes variable will reflect for the variable itself, but the changes made in non-primitive data types will be reflected in the original one as well.
+
+let myYoutubeName = "rohitmakwana.com";
+
+let anotherName = myYoutubeName;
+console.log(anotherName);
+anotherName = "techstar007";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let user1 = {
+    email: "useremail@google.com",
+    upi: "user@ybl",
+}
+
+let user2 = user1;
+
+user2.email = "user2@google.com";
+
+console.log(user1.email);
+console.log(user2.email);

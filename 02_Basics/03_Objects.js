423020983c7d9ec409e0761ca7b98e1c595350f3
r@hit_53 <95581133+rohitmak@users.noreply.github.com>
2023-11-22 18:28:12 +0000

    Object_1

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -12,7 +12,7 @@ const JSUser = {
     // the name key here is processed as "name" by the system (i.e; in the string format)
     name: "Rohit", 
     "full name": "rm",
-    mySym: "myKey1",
+    [mySym]: "myKey1", // this is how symbols are used in object using square brackets and the same way they are accessed.
     age: 19,
     location: "Indore",
     email: "rohit@google.com",
@@ -22,8 +22,26 @@ const JSUser = {
 
 // Accessing the object: Two ways: 1. Dot operator & 2. Square brackets
 
-console.log(JSUser.email);
-console.log(JSUser["email"]);
-console.log(JSUser["full name"]); // the "full name" property can be only accessed using square brackets only.
-console.log(JSUser.mySym);
-console.log(typeof JSUser.mySym);
+// console.log(JSUser.email);
+// console.log(JSUser["email"]);
+// console.log(JSUser["full name"]); // the "full name" property can be only accessed using square brackets only.
+// console.log(JSUser[mySym]);
+// console.log(typeof JSUser[mySym]);
+
+JSUser.email = "rohit@microsoft.com";
+// Object.freeze(JSUser); // this will not let any changes to be made in the object properties
+JSUser.email = "rohit@openai.com";
+
+// console.log(JSUser);
+
+JSUser.greeting = function() {
+    console.log("Hello JS User");
+}
+
+JSUser.greetingName = function() {
+    console.log(`Hello JS User ${this.name}`); // whenever you have to access the properties of the object you are in we can use 'this' operator. Here this.name will access the name of JSUser object;
+}
+
+// console.log(JSUser.greeting); // this will just return the reference of funtion, function will not be returned;
+console.log(JSUser.greeting()); // this will call the function and print the output;
+console.log(JSUser.greetingName());
